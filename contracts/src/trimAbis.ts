3c36c392d048911a118231e8497c757415b807cd
SonYoungsung <sysgigigi@gmail.com>
2024-01-31 10:59:24 +0900

    1.0.7

--- contracts/src/trimAbis.ts
+++ contracts/src/trimAbis.ts
@@ -12,11 +12,7 @@ const trimAbis = () => {
       const abiFilePath = path.join(dir, contract, 'abi', abiFileName)
       const abiFileContent = fs.readFileSync(abiFilePath, 'utf8')
       const abiObject = JSON.parse(abiFileContent)
-      //   console.log('fileContent', fileContent)
-      //   const firstFewChars = fileContent.slice(0, 10)
       if (!Array.isArray(abiObject) && abiObject.abi) {
-        console.log('test')
-        console.log(JSON.stringify(abiObject.abi, null, 2))
         fs.writeFile(
           path.join(thisAbiDirectory, abiFileName),
           JSON.stringify(abiObject.abi, null, 2),
